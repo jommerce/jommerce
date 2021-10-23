986392278e0d24f80f7a60657eaccc6d24f0c2fb
githashem <PersonalHashem@gmail.com>
2021-10-23 08:31:43 +0330

    refa: add 'django-debug-toolbar'

--- pyshem/settings/dev.py
+++ pyshem/settings/dev.py
@@ -3,6 +3,16 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
+
+# django-debug-toolbar
+assert 'django.contrib.staticfiles' in INSTALLED_APPS
+assert TEMPLATES[0]['BACKEND'] == 'django.template.backends.django.DjangoTemplates'
+assert TEMPLATES[0]['APP_DIRS']
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+INTERNAL_IPS = ['127.0.0.1']
+
+
 try:
     from .local import *
 except ImportError:
--- pyshem/urls.py
+++ pyshem/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+        urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
