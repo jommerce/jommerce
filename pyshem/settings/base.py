65a59444302afac3ab63039a460b32832d4ffa03
githashem <PersonalHashem@gmail.com>
2021-10-23 09:00:27 +0330

    refa: use custom user model

--- pyshem/settings/base.py
+++ pyshem/settings/base.py
@@ -131,3 +131,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.User'
