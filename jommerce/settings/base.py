352ca456254c59bd238df0c0aaa54c8e04301aa6
githashem <PersonalHashem@gmail.com>
2021-11-19 22:16:50 +0330

    Delete 'accounts' app

--- jommerce/settings/base.py
+++ jommerce/settings/base.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -132,6 +130,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Custom User Model
-AUTH_USER_MODEL = 'accounts.User'
