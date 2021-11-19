a82b7fbef904b6ae7e068805cf97bc14728f5bbb
githashem <PersonalHashem@gmail.com>
2021-11-19 22:22:39 +0330

    feat: use custom user model

--- jommerce/settings/base.py
+++ jommerce/settings/base.py
@@ -132,3 +132,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.User'
