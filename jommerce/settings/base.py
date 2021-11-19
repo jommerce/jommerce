8106620d9d283a2d87ece5db7ad675fece7d006d
githashem <PersonalHashem@gmail.com>
2021-11-19 22:19:52 +0330

    Initial app called 'accounts'

--- jommerce/settings/base.py
+++ jommerce/settings/base.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'jommerce.accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
