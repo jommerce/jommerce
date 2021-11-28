b9607579409b24339b546c081fcab4911ff8e05a
githashem <PersonalHashem@gmail.com>
2021-11-28 04:10:10 +0330

    Revert "add 'django-debug-toolbar'"
    This reverts commit 986392278e0d24f80f7a60657eaccc6d24f0c2fb.

--- jommerce/urls.py
+++ jommerce/urls.py
@@ -14,17 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
-from django.urls import path, include
 from django.conf import settings
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
-    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
-    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
-    if 'debug_toolbar' in settings.INSTALLED_APPS:
-        import debug_toolbar
-        urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
