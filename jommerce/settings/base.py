fd6c63ac9741c3cfe3156b1336b4356801534907
githashem <PersonalHashem@gmail.com>
2021-11-28 04:03:04 +0330

    perf!: use the 'json' library instead of 'python-decouple'
    to reduce the number of packages required

--- jommerce/settings/base.py
+++ jommerce/settings/base.py
@@ -10,20 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from decouple import config
 from pathlib import Path
-import os
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 JOMMERCE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path.cwd()
 
+# Load 'secrets.json' file
+with open(BASE_DIR / 'secrets.json', 'r') as file:
+    SECRETS = json.load(file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = SECRETS["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- jommerce/settings/prod.py
+++ jommerce/settings/prod.py
@@ -4,8 +4,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['jommerce.org', 'www.jommerce.org']
 
 
-STATIC_ROOT = os.environ['STATIC_ROOT']
-MEDIA_ROOT = os.environ['MEDIA_ROOT']
+STATIC_ROOT = SECRETS['STATIC_ROOT']
+MEDIA_ROOT = SECRETS['MEDIA_ROOT']
 
 
 try:
