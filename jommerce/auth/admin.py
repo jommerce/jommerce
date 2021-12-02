c9659f859ff69c4daee34963567fda8e0f376d66
githashem <PersonalHashem@gmail.com>
2021-12-02 06:54:27 +0330

    feat: add user admin

--- jommerce/auth/admin.py
+++ jommerce/auth/admin.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
