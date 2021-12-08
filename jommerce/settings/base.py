0642d88051d2d0e6ecb2f6a6bd9e643c692b1974
githashem <PersonalHashem@gmail.com>
2021-12-08 07:52:30 +0330

    perf: use 'pytomlpp' instead of 'tomlkit'

--- jommerce/settings/base.py
+++ jommerce/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import tomlkit
+import pytomlpp
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 JOMMERCE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +19,7 @@ BASE_DIR = Path.cwd()
 
 # Load 'secrets.toml' file
 with open(BASE_DIR / ".secrets", "r") as file:
-    SECRETS = tomlkit.loads(file.read())
+    SECRETS = pytomlpp.load(file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
