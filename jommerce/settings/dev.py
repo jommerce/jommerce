a6fe96cae941f7f7a1e1607decbdd7a25ac1230b
githashem <PersonalHashem@gmail.com>
2021-12-13 02:40:15 +0330

    feat: add 'django-debug-toolbar'

--- jommerce/settings/dev.py
+++ jommerce/settings/dev.py
@@ -5,6 +5,20 @@ SECRET_KEY = "fake-key"
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
+
+try:
+    import debug_toolbar
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS.append("debug_toolbar")
+    INTERNAL_IPS = ["127.0.0.1"]
+    MIDDLEWARE.insert(
+        MIDDLEWARE.index("django.middleware.common.CommonMiddleware") + 1,
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    )
+
+
 try:
     from .local import *
 except ImportError:
--- jommerce/urls.py
+++ jommerce/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path(
@@ -33,3 +33,11 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        urlpatterns += [
+            path("__debug__/", include(debug_toolbar.urls)),
+        ]
