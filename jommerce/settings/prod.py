5c14c336da22f6934379c17dccf58422f9938797
githashem <PersonalHashem@gmail.com>
2021-12-13 04:26:29 +0330

    feat: add database settings to the production environment

--- jommerce/settings/prod.py
+++ jommerce/settings/prod.py
@@ -3,6 +3,17 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split()
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
+    }
+}
+
 STATIC_ROOT = os.environ["STATIC_ROOT"]
 MEDIA_ROOT = os.environ["MEDIA_ROOT"]
 
