0490b30686e7e6647c851ea160d10fe0047ba8ea
githashem <PersonalHashem@gmail.com>
2021-12-17 17:42:04 +0330

    refa: add 'set_default_settings' function

--- jommerce/auth/apps.py
+++ jommerce/auth/apps.py
@@ -1,6 +1,5 @@
-from django.utils.module_loading import import_module
+from jommerce.utils import set_default_settings
 from django.apps import AppConfig
-from django.conf import settings
 
 
 class AuthConfig(AppConfig):
@@ -8,18 +7,6 @@ class AuthConfig(AppConfig):
     name = "jommerce.auth"
     label = "jauth"
 
-    def __init__(self, app_name, app_module):
-        super().__init__(app_name, app_module)
-
-        try:
-            conf = import_module(self.name + ".settings")
-        except ImportError:
-            pass
-        else:
-            for name in filter(lambda x: x.isupper(), dir(conf)):
-                if not hasattr(settings, name):
-                    setattr(
-                        settings,
-                        name,
-                        getattr(conf, name),
-                    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        set_default_settings(f"{self.name}.settings")
---
+++ jommerce/utils.py
@@ -0,0 +1,18 @@
+from django.utils.module_loading import import_module
+from django.conf import settings
+
+
+def set_default_settings(settings_module, prefix: str = ""):
+    try:
+        conf = import_module(settings_module)
+    except ImportError:
+        raise ModuleNotFoundError(f"No module named '{settings_module}'")
+    else:
+        for key in filter(lambda x: x.isupper(), dir(conf)):
+            key = prefix.upper() + key
+            if not hasattr(settings, key):
+                setattr(
+                    settings,
+                    key,
+                    getattr(conf, key[len(prefix) :]),
+                )
