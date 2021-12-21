1434057b30c93a8509653ae45599743a1c8d96d9
githashem <PersonalHashem@gmail.com>
2021-12-21 19:12:18 +0330

    refa: import 'debug_toolbar' when in 'INSTALLED_APPS'

--- jommerce/urls.py
+++ jommerce/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if "debug_toolbar" in settings.INSTALLED_APPS:
     try:
         import debug_toolbar
     except ImportError:
