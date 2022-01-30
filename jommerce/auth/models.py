7ea6c64848bcef9a3810804ae26d22061ffcb80d
githashem <PersonalHashem@gmail.com>
2022-01-30 20:15:23 +0330

    feat(auth): add superuser

--- jommerce/auth/models.py
+++ jommerce/auth/models.py
@@ -1,7 +1,12 @@
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 from django.db import models
 
 
 class User(models.Model):
     email = models.EmailField(_("email"), max_length=64, unique=True)
     password = models.CharField(_("password"), max_length=128)
+
+    @property
+    def is_superuser(self):
+        return settings.AUTH_SUPERUSER_ID == self.id
