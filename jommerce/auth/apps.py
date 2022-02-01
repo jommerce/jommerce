46309816184e91fcf855902bd273de7b1c7fdf61
githashem <PersonalHashem@gmail.com>
2022-02-01 14:26:06 +0330

    feat(auth): add app verbose_name

--- jommerce/auth/apps.py
+++ jommerce/auth/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "jommerce.auth"
+    verbose_name = _("Authentication and Authorization")
 
     def ready(self):
         from jommerce.auth import checks
