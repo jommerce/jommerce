518f10f0860a12b3cd4ff17f1a8effc87740f716
githashem <PersonalHashem@gmail.com>
2022-02-01 18:31:22 +0330

    feat: change DEFAULT_AUTO_FIELD to 'django.db.models.AutoField'

--- jommerce/settings/base.py
+++ jommerce/settings/base.py
@@ -138,4 +138,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
