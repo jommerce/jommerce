ca05f6961b53d8ab51411feeb463a42ac172682a
githashem <PersonalHashem@gmail.com>
2022-02-04 06:57:36 +0330

    refa(auth): add user verbose_name and change email verbose_name

--- jommerce/auth/models.py
+++ jommerce/auth/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 
 class User(models.Model):
-    email = models.EmailField(_("email"), max_length=64, unique=True)
+    email = models.EmailField(_("email address"), max_length=64, unique=True)
     password = models.CharField(_("password"), max_length=128)
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
 
     @property
     def is_superuser(self):
