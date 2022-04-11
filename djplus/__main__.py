6e6109381791525064217c4e924407650d8dd857
githashem <PersonalHashem@gmail.com>
2022-04-11 17:02:46 +0430

    ENH: add the project custom template

--- djplus/__main__.py
+++ djplus/__main__.py
@@ -1,10 +1,15 @@
 from django.core.management import execute_from_command_line
+from pathlib import Path
+
+DIR_PROJECT_TEMPLATE = Path(__file__).parent / "project_template"
 
 
 def main():
     execute_from_command_line([
         "django-admin",
         "startproject",
+        "--template",
+        str(DIR_PROJECT_TEMPLATE),
         "config",
         ".",
     ])
