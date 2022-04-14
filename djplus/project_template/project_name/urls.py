768c9aef07d93e8f2422a5a3f1706d2fc360a1b1
githashem <PersonalHashem@gmail.com>
2022-04-14 15:02:48 +0430

    ENH: add home page

--- djplus/project_template/project_name/urls.py
+++ djplus/project_template/project_name/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import IndexView
 
 urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
     path('admin/', admin.site.urls),
 ]
 
