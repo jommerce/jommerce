7a436d3f97938e020855ad40cd67b2ee3849e8ea
githashem <PersonalHashem@gmail.com>
2022-04-30 06:02:03 +0430

    TST: add `check_password` test

--- tests/auth/test_models.py
+++ tests/auth/test_models.py
@@ -7,6 +7,7 @@ class UserModelTest(TestCase):
     def setUpTestData(cls):
         User.objects.create(email="user1@gmail.com", password="123456")
         User.objects.create(email="user2@gmail.com", password="123456")
+        User.objects.create(email="user3@gmail.com", password="password")
 
     def test_password_hashing_with_salt(self):
         user1 = User.objects.get(pk=1)
@@ -14,6 +15,15 @@ class UserModelTest(TestCase):
         user2 = User.objects.get(pk=2)
         self.assertNotEqual(user2.password, "123456")
         self.assertNotEqual(user1.password, user2.password)
+
+    def test_check_password(self):
+        user = User.objects.get(pk=1)
+        self.assertTrue(user.check_password("123456"))
+        self.assertFalse(user.check_password("password"))
+
+        user = User.objects.get(pk=3)
+        self.assertTrue(user.check_password("password"))
+        self.assertFalse(user.check_password("123456"))
 
     def test_is_authenticated(self):
         user1 = User.objects.get(pk=1)
