c12354c6d854894987a8874557b9ef30ba2194a5
githashem <PersonalHashem@gmail.com>
2022-04-30 05:21:28 +0430

    ENH: add `User` model

--- djplus/auth/models.py
+++ djplus/auth/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=64, unique=True)
+    password = models.CharField(max_length=128)
