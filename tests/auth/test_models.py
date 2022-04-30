3c21aca5a1c0dd5ffb5f4f8b5afce5ab6893e96f
githashem <PersonalHashem@gmail.com>
2022-04-30 05:51:48 +0430

    TST: add `is_authenticated` and `is_anonymous` tests

--- tests/auth/test_models.py
+++ tests/auth/test_models.py
@@ -9,3 +9,15 @@ class UserModelTest(TestCase):
         user2 = User.objects.create(email="user2@gmail.com", password="123456")
         self.assertNotEqual(user2.password, "123456")
         self.assertNotEqual(user1.password, user2.password)
+
+    def test_is_authenticated(self):
+        user1 = User.objects.create(email="user1@gmail.com", password="123456")
+        self.assertTrue(user1.is_authenticated)
+        user2 = User.objects.create(email="user2@gmail.com", password="123456")
+        self.assertTrue(user2.is_authenticated)
+
+    def test_is_anonymous(self):
+        user1 = User.objects.create(email="user1@gmail.com", password="123456")
+        self.assertFalse(user1.is_anonymous)
+        user2 = User.objects.create(email="user2@gmail.com", password="123456")
+        self.assertFalse(user2.is_anonymous)
