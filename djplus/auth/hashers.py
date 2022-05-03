00c6abaf07d8e967d8562ef1b33c362029f7f125
githashem <PersonalHashem@gmail.com>
2022-05-03 17:51:26 +0430

    REF: inclusion of special characters in salt

--- djplus/auth/hashers.py
+++ djplus/auth/hashers.py
@@ -2,4 +2,5 @@ import secrets
 
 
 def generate_salt():
-    return "".join(secrets.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(32))
+    characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    return "".join(secrets.choice(characters) for _ in range(32))
