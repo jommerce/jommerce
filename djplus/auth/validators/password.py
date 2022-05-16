15c989dd70debafa405a18fc00159d1ee6e255fc
githashem <PersonalHashem@gmail.com>
2022-05-17 01:29:37 +0430

    ENH: minimum and maximum password length can be `None`

--- djplus/auth/validators/password.py
+++ djplus/auth/validators/password.py
@@ -6,19 +6,28 @@ from django.utils.deconstruct import deconstructible
 @deconstructible
 class PasswordLengthValidator:
     def __init__(self, min_length=8, max_length=100):
+        if min_length is None and max_length is None:
+            raise ValueError("Both 'min_length' and 'max_length' arguments cannot be None")
+        if min_length is not None and min_length <= 0:
+            raise ValueError("The 'min_length' argument must be greater than zero or None")
+        if max_length is not None and max_length <= 0:
+            raise ValueError("The 'max_length' argument must be greater than zero or None")
+        if min_length is not None and max_length is not None and min_length > max_length:
+            raise ValueError("The 'min_length' argument must be less than the 'max_length' argument")
+
         self.min_length = min_length
         self.max_length = max_length
 
     def __call__(self, password):
-        if len(password) < self.min_length:
+        if self.min_length and len(password) < self.min_length:
             raise ValidationError(
-                _("at least %(min_length)d characters"),
+                _("Your password must be at least %(min_length)d characters long."),
                 code="password_too_short",
                 params={"min_length": self.min_length},
             )
-        elif self.max_length < len(password):
+        elif self.max_length and self.max_length < len(password):
             raise ValidationError(
-                _("at most %(max_length)d characters"),
+                _("Your password must be at most %(max_length)d characters long."),
                 code="password_too_long",
                 params={"max_length": self.max_length},
             )
