94c3af9226d0ff3224e496fc89cee94d2ee31ee5
githashem <PersonalHashem@gmail.com>
2022-05-17 01:26:40 +0430

    TST: use stronger test to validate the password length

--- tests/auth/test_validators.py
+++ tests/auth/test_validators.py
@@ -60,40 +60,6 @@ class PasswordValidatorsTest(TestCase):
         self.assertIsNone(validate("s6cm2%S30y"))
         self.assertIsNone(validate("s5FD#fs!4$3"))
 
-    def test_validate_password_length(self):
-        validate_between_8_and_16_characters = PasswordLengthValidator(min_length=8, max_length=16)
-        validate_between_6_and_20_characters = PasswordLengthValidator(min_length=6, max_length=20)
-
-        with self.assertRaisesMessage(ValidationError, "at least 8 characters") as err:
-            validate_between_8_and_16_characters("T$0@aYs")
-        self.assertEqual(err.exception.code, "password_too_short")
-
-        with self.assertRaisesMessage(ValidationError, "at most 16 characters") as err:
-            validate_between_8_and_16_characters('Bh:d]4G9i*5Y-dv?k>t%&c')
-        self.assertEqual(err.exception.code, "password_too_long")
-
-        with self.assertRaisesMessage(ValidationError, "at least 6 characters") as err:
-            validate_between_6_and_20_characters("Nm6$")
-        self.assertEqual(err.exception.code, "password_too_short")
-
-        with self.assertRaisesMessage(ValidationError, "at most 20 characters") as err:
-            validate_between_6_and_20_characters('mk%*G;-(&T_?^x=Z-hw0DD')
-        self.assertEqual(err.exception.code, "password_too_long")
-
-        self.assertIsNone(validate_between_8_and_16_characters("m$dSC0#8"))
-        self.assertIsNone(validate_between_8_and_16_characters("ml|&6UErZk{&"))
-        self.assertIsNone(validate_between_8_and_16_characters("N`{@T%!/z_~*22rm"))
-
-        self.assertIsNone(validate_between_6_and_20_characters("<{2|N&"))
-        self.assertIsNone(validate_between_6_and_20_characters("*q[*%'=k9kRo{a!_TI"))
-        self.assertIsNone(validate_between_6_and_20_characters("m$@dFG(*3_0!No^$#i-b"))
-
-        validate_between_8_and_20_characters = PasswordLengthValidator(min_length=8, max_length=20)
-        self.assertNotEqual(validate_between_8_and_16_characters, validate_between_8_and_20_characters)
-        self.assertNotEqual(validate_between_6_and_20_characters, validate_between_8_and_20_characters)
-        self.assertNotEqual(validate_between_6_and_20_characters, validate_between_8_and_16_characters)
-        self.assertEqual(validate_between_8_and_16_characters, PasswordLengthValidator(min_length=8, max_length=16))
-
 
 class UsernameValidatorsTest(TestCase):
     def test_get_username_validators(self):
@@ -194,3 +160,91 @@ class UsernameLengthValidatorTests(TestCase):
         self.assertEqual(validate_between_5_and_32_characters, UsernameLengthValidator(min_length=5, max_length=32))
         self.assertEqual(validate_at_least_5_characters, UsernameLengthValidator(min_length=5, max_length=None))
         self.assertEqual(validate_at_most_32_characters, UsernameLengthValidator(min_length=None, max_length=32))
+
+
+class PasswordLengthValidatorTests(TestCase):
+    def test_raise_when_min_length_is_equal_to_or_less_than_zero(self):
+        expected_message = "The 'min_length' argument must be greater than zero or None"
+        for num in (0, -1, -2):
+            with self.assertRaisesMessage(ValueError, expected_message):
+                PasswordLengthValidator(min_length=num)
+
+    def test_raise_when_max_length_is_equal_to_or_less_than_zero(self):
+        expected_message = "The 'max_length' argument must be greater than zero or None"
+        for num in (0, -1, -2):
+            with self.assertRaisesMessage(ValueError, expected_message):
+                PasswordLengthValidator(max_length=num)
+
+    def test_raise_when_min_length_greater_than_max_length(self):
+        expected_message = "The 'min_length' argument must be less than the 'max_length' argument"
+        with self.assertRaisesMessage(ValueError, expected_message):
+            PasswordLengthValidator(min_length=100, max_length=8)
+        with self.assertRaisesMessage(ValueError, expected_message):
+            PasswordLengthValidator(min_length=6, max_length=5)
+
+    def test_raise_when_both_min_length_and_max_length_arguments_are_none(self):
+        expected_message = "Both 'min_length' and 'max_length' arguments cannot be None"
+        with self.assertRaisesMessage(ValueError, expected_message):
+            PasswordLengthValidator(min_length=None, max_length=None)
+
+    def test_validate_with_min_length_and_max_length(self):
+        validate_between_8_and_100_characters = PasswordLengthValidator(min_length=8, max_length=100)
+
+        expected_message = "Your password must be at least 8 characters long."
+        with self.assertRaisesMessage(ValidationError, expected_message) as err:
+            validate_between_8_and_100_characters("T$0@aYs")
+        self.assertEqual(err.exception.code, "password_too_short")
+
+        expected_message = "Your password must be at most 100 characters long."
+        with self.assertRaisesMessage(ValidationError, expected_message) as err:
+            validate_between_8_and_100_characters("a" * 101)
+        self.assertEqual(err.exception.code, "password_too_long")
+
+        self.assertIsNone(validate_between_8_and_100_characters("password"))
+        self.assertIsNone(validate_between_8_and_100_characters("l_dfj$34#lkjfs434_*sl3"))
+        self.assertIsNone(validate_between_8_and_100_characters("a" * 100))
+
+    def test_validate_just_with_max_length(self):
+        validate_at_most_10_characters = PasswordLengthValidator(min_length=None, max_length=10)
+
+        expected_message = "Your password must be at most 10 characters long."
+        with self.assertRaisesMessage(ValidationError, expected_message) as err:
+            validate_at_most_10_characters("a" * 11)
+        self.assertEqual(err.exception.code, "password_too_long")
+
+        self.assertIsNone(validate_at_most_10_characters(""))
+        self.assertIsNone(validate_at_most_10_characters("django"))
+        self.assertIsNone(validate_at_most_10_characters("b" * 10))
+
+    def test_validate_just_with_min_length(self):
+        validate_at_least_5_characters = PasswordLengthValidator(min_length=5, max_length=None)
+
+        expected_message = "Your password must be at least 5 characters long."
+        with self.assertRaisesMessage(ValidationError, expected_message) as err:
+            validate_at_least_5_characters("test")
+        self.assertEqual(err.exception.code, "password_too_short")
+
+        self.assertIsNone(validate_at_least_5_characters("admin"))
+        self.assertIsNone(validate_at_least_5_characters("javascript"))
+        self.assertIsNone(validate_at_least_5_characters("c" * 255))
+
+    def test_equal_method(self):
+        validate_between_5_and_32_characters = PasswordLengthValidator(min_length=5, max_length=32)
+        validate_at_most_32_characters = PasswordLengthValidator(min_length=None, max_length=32)
+        validate_at_least_5_characters = PasswordLengthValidator(min_length=5, max_length=None)
+
+        self.assertNotEqual(validate_between_5_and_32_characters, validate_at_least_5_characters)
+        self.assertNotEqual(validate_between_5_and_32_characters, validate_at_most_32_characters)
+        self.assertNotEqual(validate_at_least_5_characters, validate_at_most_32_characters)
+
+        validate_between_6_and_32_characters = PasswordLengthValidator(min_length=6, max_length=32)
+        validate_between_5_and_30_characters = PasswordLengthValidator(min_length=5, max_length=30)
+        validate_between_6_and_30_characters = PasswordLengthValidator(min_length=6, max_length=30)
+
+        self.assertNotEqual(validate_between_5_and_32_characters, validate_between_6_and_32_characters)
+        self.assertNotEqual(validate_between_5_and_32_characters, validate_between_5_and_30_characters)
+        self.assertNotEqual(validate_between_5_and_32_characters, validate_between_6_and_30_characters)
+
+        self.assertEqual(validate_between_5_and_32_characters, PasswordLengthValidator(min_length=5, max_length=32))
+        self.assertEqual(validate_at_least_5_characters, PasswordLengthValidator(min_length=5, max_length=None))
+        self.assertEqual(validate_at_most_32_characters, PasswordLengthValidator(min_length=None, max_length=32))
