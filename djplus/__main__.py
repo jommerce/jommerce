bd5e27f650ad05c29aa8e10bc680a5c1889a2087
githashem <PersonalHashem@gmail.com>
2022-06-26 13:17:59 +0430

    PER: improve the execution speed of the `djplus` command

--- djplus/__main__.py
+++ djplus/__main__.py
@@ -1,4 +1,4 @@
-from django.core.management import execute_from_command_line
+from django.core.management.templates import TemplateCommand
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
@@ -6,17 +6,16 @@ PROJECT_TEMPLATE_DIR = Path(__file__).parent / "project_template"
 
 
 def main():
-    project_name = input("project_name [config]: ").strip() or "config"
-    execute_from_command_line([
-        "django-admin",
-        "startproject",
-        "--template",
-        str(PROJECT_TEMPLATE_DIR),
-        "--name",
-        ".gitignore",
-        str(project_name),
-        ".",
-    ])
+    TemplateCommand().handle(
+        "project",
+        name=input("project_name[config]: ").strip() or "config",
+        target=str(Path.cwd()),
+        template=str(PROJECT_TEMPLATE_DIR),
+        files=[".gitignore"],
+        extensions=[".py", ".py-tpl"],
+        exclude=[],
+        verbosity=0,
+    )
     print(f'SECRET_KEY = "{get_random_secret_key()}"')
 
 
