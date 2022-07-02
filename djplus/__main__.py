baa95209828abcaa2d1ec4ca885f252720d09590
githashem <PersonalHashem@gmail.com>
2022-07-02 11:36:01 +0430

    ENH: customize project template using `djplus.json` file
    Closes #1

--- djplus/__main__.py
+++ djplus/__main__.py
@@ -1,11 +1,18 @@
 from django.core.management.templates import TemplateCommand
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
+import json
 
 PROJECT_TEMPLATE_DIR = Path(__file__).parent / "project_template"
 
 
 def main():
+    try:
+        with open(Path.home() / "djplus.json") as file:
+            config = json.load(file)
+    except FileNotFoundError:
+        config = {}
+
     TemplateCommand().handle(
         "project",
         name=input("project_name[config]: ").strip() or "config",
@@ -17,6 +24,7 @@ def main():
         verbosity=0,
 
         secret_key=get_random_secret_key(),
+        **config,
     )
 
 
