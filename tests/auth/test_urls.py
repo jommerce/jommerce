597d2437b0d0a750ac181a14aba324c52b82e061
githashem <PersonalHashem@gmail.com>
2022-07-06 19:33:03 +0430

    TST: add named url tests

---
+++ tests/auth/test_urls.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, override_settings
+from django.urls import reverse, NoReverseMatch
+
+
+@override_settings(ROOT_URLCONF="djplus.auth.urls")
+class NamedUrlsTest(TestCase):
+    def test_login(self):
+        try:
+            self.assertEqual(reverse("login"), "/login/")
+        except NoReverseMatch:
+            self.fail("Reversal of url named 'login' failed with NoReverseMatch")
+
+    def test_logout(self):
+        try:
+            self.assertEqual(reverse("logout"), "/logout/")
+        except NoReverseMatch:
+            self.fail("Reversal of url named 'logout' failed with NoReverseMatch")
+
+    def test_register(self):
+        try:
+            self.assertEqual(reverse("register"), "/register/")
+        except NoReverseMatch:
+            self.fail("Reversal of url named 'register' failed with NoReverseMatch")
