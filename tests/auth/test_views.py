d47ac4dbc63455c69b3d3fd6af182d973d097f66
githashem <PersonalHashem@gmail.com>
2022-07-11 20:13:12 +0430

    TST: add `AUTH_LOGIN_REDIRECT_URL` test

--- tests/auth/test_views.py
+++ tests/auth/test_views.py
@@ -7,7 +7,23 @@ from djplus.auth.models import User
 
 @override_settings(ROOT_URLCONF="djplus.auth.urls")
 class LoginViewTest(TestCase):
-    pass
+    user_data = {
+        "username": "test",
+        "password": "123456",
+    }
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(**cls.user_data)
+
+    def setUp(self) -> None:
+        self.client.post("/logout/")
+
+    def test_login_redirect_url_setting(self):
+        for url in {"/custom/", "/"}:
+            with self.subTest(AUTH_LOGIN_REDIRECT_URL=url), self.settings(AUTH_LOGIN_REDIRECT_URL=url):
+                response = self.client.post("/login/", data=self.user_data)
+                self.assertRedirects(response, url, fetch_redirect_response=False)
 
 
 @override_settings(ROOT_URLCONF="djplus.auth.urls")
