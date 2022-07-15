ce6bba8e87e38b1bee08d12f85e246ddfea67f82
githashem <PersonalHashem@gmail.com>
2022-07-15 20:37:51 +0430

    TST: test access to the logout page when user is anonymous

--- tests/auth/test_views.py
+++ tests/auth/test_views.py
@@ -178,3 +178,29 @@ class AccessLogoutPageWhenUserIsAuthenticated(TestCase):
     def test_no_redirect(self):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, "auth/logout.html")
+
+
+@override_settings(ROOT_URLCONF="djplus.auth.urls")
+@modify_settings(MIDDLEWARE={"append": "djplus.auth.middleware.AuthenticationMiddleware"})
+class AccessLogoutPageWhenUserIsAnonymous(TestCase):
+    def setUp(self) -> None:
+        self.client.post("/logout/")
+        self.response = self.client.get("/logout/")
+
+    def test_do_not_use_logout_template(self):
+        self.assertTemplateNotUsed(self.response, "auth/logout.html")
+
+    def test_status_code(self):
+        self.assertEqual(self.response.status_code, 302)
+
+    @override_settings(AUTH_LOGOUT_REDIRECT_URL="/custom/")
+    def test_redirect_to_a_custom_url(self):
+        self.assertRedirects(self.response, "/custom/", fetch_redirect_response=False)
+
+    @override_settings(AUTH_LOGOUT_REDIRECT_URL="/")
+    def test_redirect_to_home_page(self):
+        self.assertRedirects(self.response, "/", fetch_redirect_response=False)
+
+    @override_settings(AUTH_LOGOUT_REDIRECT_URL=None)
+    def test_redirect_to_home_page_by_default(self):
+        self.assertRedirects(self.response, "/", fetch_redirect_response=False)
