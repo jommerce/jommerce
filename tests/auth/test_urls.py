2490250d82df24caffd10da3ecb89df29b99032b
githashem <PersonalHashem@gmail.com>
2022-07-15 16:32:48 +0430

    REF: rename `NamedUrlsTest` and its methods

--- tests/auth/test_urls.py
+++ tests/auth/test_urls.py
@@ -3,20 +3,20 @@ from django.urls import reverse, NoReverseMatch
 
 
 @override_settings(ROOT_URLCONF="djplus.auth.urls")
-class NamedUrlsTest(TestCase):
-    def test_login(self):
+class AccessUrlsByName(TestCase):
+    def test_access_login_url(self):
         try:
             self.assertEqual(reverse("login"), "/login/")
         except NoReverseMatch:
             self.fail("Reversal of url named 'login' failed with NoReverseMatch")
 
-    def test_logout(self):
+    def test_access_logout_url(self):
         try:
             self.assertEqual(reverse("logout"), "/logout/")
         except NoReverseMatch:
             self.fail("Reversal of url named 'logout' failed with NoReverseMatch")
 
-    def test_signup(self):
+    def test_access_signup_url(self):
         try:
             self.assertEqual(reverse("signup"), "/signup/")
         except NoReverseMatch:
