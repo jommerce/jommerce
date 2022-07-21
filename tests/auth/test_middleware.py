da609b17c3d0e8579a22eb2473f44b41de62a4bc
githashem <PersonalHashem@gmail.com>
2022-07-21 17:47:55 +0430

    TST: test session settings

--- tests/auth/test_middleware.py
+++ tests/auth/test_middleware.py
@@ -61,3 +61,77 @@ class IdentifyUser(TestCase):
         self.assertEqual(self.request.user, user)
         self.assertEqual(self.request.session, session)
         self.assertEqual(self.request.session.user, user)
+
+
+class SessionSettings(TestCase):
+    def setUp(self) -> None:
+        self.request = RequestFactory().get("/")
+        self.middleware = AuthenticationMiddleware(lambda req: HttpResponse())
+
+    def test_secure_session_cookie(self):
+        with self.settings(AUTH_SESSION_COOKIE_SECURE=True):
+            response = self.middleware(self.request)
+            self.assertTrue(response.cookies[SESSION_COOKIE_NAME]["secure"])
+        with self.settings(AUTH_SESSION_COOKIE_SECURE=False):
+            response = self.middleware(self.request)
+            self.assertFalse(response.cookies[SESSION_COOKIE_NAME]["secure"])
+
+    def test_httponly_session_cookie(self):
+        with self.settings(AUTH_SESSION_COOKIE_HTTPONLY=True):
+            response = self.middleware(self.request)
+            self.assertTrue(response.cookies[SESSION_COOKIE_NAME]["httponly"])
+        with self.settings(AUTH_SESSION_COOKIE_HTTPONLY=False):
+            response = self.middleware(self.request)
+            self.assertFalse(response.cookies[SESSION_COOKIE_NAME]["httponly"])
+
+    def test_samesite_session_cookie(self):
+        with self.settings(AUTH_SESSION_COOKIE_SAMESITE="Strict"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["samesite"], "Strict")
+        with self.settings(AUTH_SESSION_COOKIE_SAMESITE="Lax"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["samesite"], "Lax")
+        with self.settings(AUTH_SESSION_COOKIE_SAMESITE="None"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["samesite"], "None")
+        with self.settings(AUTH_SESSION_COOKIE_SAMESITE=False):
+            response = self.middleware(self.request)
+            self.assertFalse(response.cookies[SESSION_COOKIE_NAME]["samesite"])
+
+    def test_domain_session_cookie(self):
+        with self.settings(AUTH_SESSION_COOKIE_DOMAIN=".example.com"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["domain"], ".example.com")
+        with self.settings(AUTH_SESSION_COOKIE_DOMAIN="example.local"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["domain"], "example.local")
+        with self.settings(AUTH_SESSION_COOKIE_DOMAIN=None):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["domain"], "")
+
+    def test_path_session_cookie(self):
+        with self.settings(AUTH_SESSION_COOKIE_PATH="/"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["path"], "/")
+        with self.settings(AUTH_SESSION_COOKIE_PATH="/custom/"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["path"], "/custom/")
+        with self.settings(AUTH_SESSION_COOKIE_PATH="/example/"):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["path"], "/example/")
+
+    def test_age_session_cookie(self):
+        with self.settings(AUTH_SESSION_COOKIE_AGE=10):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["max-age"], 10)
+        with self.settings(AUTH_SESSION_COOKIE_AGE=60 * 60 * 24):
+            response = self.middleware(self.request)
+            self.assertEqual(response.cookies[SESSION_COOKIE_NAME]["max-age"], 86400)
+
+    def test_name_session_cookie(self):
+        with self.settings(AUTH_SESSION_COOKIE_NAME="custom"):
+            response = self.middleware(self.request)
+            self.assertIn("custom", response.cookies)
+        with self.settings(AUTH_SESSION_COOKIE_NAME="session_key"):
+            response = self.middleware(self.request)
+            self.assertIn("session_key", response.cookies)
