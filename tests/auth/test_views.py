217f1b32278b7d218652c9f58d1deb9d152eed05
githashem <PersonalHashem@gmail.com>
2022-07-23 12:12:34 +0430

    TST: test sign up user in database

--- tests/auth/test_views.py
+++ tests/auth/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, override_settings
+from djplus.auth.models import User
 
 
 @override_settings(ROOT_URLCONF="djplus.auth.urls")
@@ -20,3 +21,10 @@ class SignupViewTests(TestCase):
     def test_template_name_for_signup_page(self):
         response = self.client.get("/signup/")
         self.assertTemplateUsed(response, "auth/signup.html")
+
+    def test_sign_up_user_in_the_database(self):
+        self.client.post("/signup/", data={"email": "staff@domain.com", "password": "password"})
+        try:
+            User.objects.get(email="staff@domain.com")
+        except User.DoesNotExist:
+            self.fail("The desired user is not saved in the database.")
