1b65a1b3a67c93ea3061696bd1467c2a79123df7
githashem <PersonalHashem@gmail.com>
2022-07-23 11:46:22 +0430

    TST: test template name for signup page

--- tests/auth/test_views.py
+++ tests/auth/test_views.py
@@ -13,3 +13,10 @@ class LogoutViewTests(TestCase):
     def test_template_name_for_logout_page(self):
         response = self.client.get("/logout/")
         self.assertTemplateUsed(response, "auth/logout.html")
+
+
+@override_settings(ROOT_URLCONF="djplus.auth.urls")
+class SignupViewTests(TestCase):
+    def test_template_name_for_signup_page(self):
+        response = self.client.get("/signup/")
+        self.assertTemplateUsed(response, "auth/signup.html")
