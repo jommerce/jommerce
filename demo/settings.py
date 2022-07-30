1efe0f9b0934926b0788f658ee564b674fb663df
githashem <PersonalHashem@gmail.com>
2022-07-30 17:30:10 +0430

    Add `blog` app to `INSTALLED_APPS`

--- demo/settings.py
+++ demo/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     # "django.contrib.admin",
     # "django.contrib.auth",
     "djplus.auth",
+    "djplus.blog",
     # "django.contrib.contenttypes",
     # "django.contrib.sessions",
     # "django.contrib.messages",
