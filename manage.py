3099e12e53ecc079b2f5120d70c2efaf07187185
githashem <PersonalHashem@gmail.com>
2023-02-06 07:27:58 +0330

    Clean up the codes

--- manage.py
+++ manage.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-
-def main():
-    """Run administrative tasks."""
-    if os.path.isfile("dj/project/settings/local.py"):
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj.project.settings.local')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj.project.settings.dev')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+from django.core.management import execute_from_command_line
 
 
 if __name__ == '__main__':
-    main()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj.project.settings.dev')
+    execute_from_command_line(sys.argv)
