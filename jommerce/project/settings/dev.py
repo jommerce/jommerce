ba54319696e1a224d3042ddde78c722c69c5c8be
Mohammad Hashem Nowruzi <82329484+githashem@users.noreply.github.com>
2023-02-16 05:54:25 +0330

    Import local settings

--- jommerce/project/settings/dev.py
+++ jommerce/project/settings/dev.py
@@ -16,3 +16,9 @@ if find_spec("debug_toolbar"):
         "SHOW_TEMPLATE_CONTEXT": True,
     }
     INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+
+    
+try:
+    from .local import *
+except ImportError:
+    pass
