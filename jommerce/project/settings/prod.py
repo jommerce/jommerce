45829202a1d2d96b355f079847be824d1fb09a71
Mohammad Hashem Nowruzi <82329484+githashem@users.noreply.github.com>
2023-02-16 05:55:10 +0330

    Import local settings

--- jommerce/project/settings/prod.py
+++ jommerce/project/settings/prod.py
@@ -5,3 +5,10 @@ from .base import *
 # ----------------------------------------------------------------------------------------------------------------------
 DEBUG = False
 ALLOWED_HOSTS += []
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
+  
